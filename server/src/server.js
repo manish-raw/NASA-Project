6bfff2ba388f075cf67a53032314486d2bb52c3e
manish-raw <manishyadav.y31@gmail.com>
2025-04-19 14:33:41 +0530

    load planets in FE

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,18 @@
 const http = require('http');   
 const app = require('./app');
+const { loadPlanetsData } = require('./models/planets.model')
+
 const PORT = process.env.PORT || 8000; 
 
 const server = http.createServer(app);
 
 
-
-server.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);  
-})
+// Load the planets data before starting the server
+// so that the data is available when the server starts
+async function startServer(){
+    await loadPlanetsData();
+    server.listen(PORT, ()=>{
+        console.log(`Server is running on port ${PORT}`);  
+    });
+}
+startServer();
