7b5b2a5ae3402e4dba57a5b43720e4d5447eecd8
manish-raw <manishyadav.y31@gmail.com>
2025-04-26 23:37:42 +0530

    addNewLaunch functionality

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,12 +1,14 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch ={
     flightNumber: 100,
     mission: 'Kepler Exploration X',
     rocket: 'Explorer IS1',
     launchDate: new Date('December 27, 2030'),
     destination: 'Kepler-442 b',
-    customers: ['NASA', 'ZTM'],
+    customers: ['Manas', 'Manish'],
     upcoming: true,
     success: true,
 }
@@ -17,6 +19,18 @@ function getAllLaunches(){
     return Array.from(launches.values());
 }
 
+function addNewLaunch(launch){
+    latestFlightNumber++;
+    launches.set(latestFlightNumber, Object.assign(launch, {
+        flightNumber: latestFlightNumber,
+        customers: ['MAY', 'ARY'],
+        upcoming: true,
+        success: true,
+        })
+    );
+}
+
 module.exports = {
     getAllLaunches,
+    addNewLaunch,
 }
